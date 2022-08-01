fb1a044420c4d6b326c88914fb5ef8eba218e8b8
Joseph Poole <Joseph.poole90@gmail.com>
2022-08-01 14:12:04 -0500

    exercise 1 and 2 completed

---
+++ BestBuyBestPractices/BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyBestPractices
+{
+    internal class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            name = "Divinity Original sin";
+            price = 54.99;
+            categoryID = 8;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return (IEnumerable<Product>)_connection.Query<Product>("SELECT * FROM products;");
+        }
+    }
+}
--- BestBuyBestPractices/BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/BestBuyBestPractices/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.IO;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using BestBuyBestPractices;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var repo = new DepartmentRepository(conn);
+var repo2 = new DapperProductRepository(conn);
+
+repo.InsertDepartment("My new Department!");
+
+var departments = repo.GetAllDepartments();
+
+foreach (var department in departments)
+{
+    Console.WriteLine(department.DepartmentID);
+    Console.WriteLine(department.Name);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+repo2.CreateProduct("Divinity Original Sin", 54.99, 8);
+
+var products = repo2.GetAllProducts();
+
+foreach (var product in products)
+{
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.categoryID);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine();
+    Console.WriteLine();
+}
